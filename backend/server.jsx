8526503426f364ad9bc6a559226c720fd4d3bbca
nandu <nandub719@gmail.com>
2025-02-03 11:19:17 +0530

    upload

---
+++ backend/server.jsx
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to handle errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hero from "./components/hero";
+import Navigation from "./components/navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <Hero/>
+    </div>
+  );
+};
+
+export default App;
